11c724f1bbf81d54fccc0b86ce26f5c2d06a0189
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-02 14:21:54 +0200

    Add → added my function section in JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 console.log('JS Test');
 
+// Esercizio 1
+/*
+    Data una stringa, restituire la versione in alfabeto farfallino (le vocali devono essere trasformate in questo modo: a -> afa, e -> efe, i -> ifi, o -> ofo, u- > ufu)
+*/
 
+
+
+
+
+
+
+
+
+
+// ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ Mie Funzioni ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ \\
+
+
+// ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ Mie Funzioni ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ \\
