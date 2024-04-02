439030f2d09bf8a35c3c94cc5ebc0920dcae5e9c
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-02 14:57:35 +0200

    Feat → added function that given a string returns letterers between two given index

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
 console.log('JS Test');
 
-// Esercizio 1
+// Esercizio 1 → Cifrario 
 /*
     Data una stringa, restituire la versione in alfabeto farfallino (le vocali devono essere trasformate in questo modo: a -> afa, e -> efe, i -> ifi, o -> ofo, u- > ufu)
 */
 
-const string = 'a e i o u'
-console.log(string)
+const stringCifrario = 'a e i o u'
+console.log(stringCifrario)
 
 
 
-console.log(butterlfyLetters(string))
+console.log(butterlfyLetters(stringCifrario))
+
+
+
+
+
+
+// Esercizio 2 → Stringa "infinita"
+/*
+    Data una stringa con un pattern ripetuto (es. ABCDE), ipoteticamente infinita, restituire le lettere racchiuse fra due indici (es. 13 e 16) inclusi.
+*/
+
+const stringaIniziale = 'ABCDE'
+
+const stringaInfinita = stringaIniziale.repeat(9999)
+
+
+console.log(lettersOfInfiniteString(stringaInfinita, 13, 16));
+
+
+
+
+
 
 
 
@@ -18,6 +40,7 @@ console.log(butterlfyLetters(string))
 
 // ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ Mie Funzioni ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ \\
 
+// funzione per trasformare una parola nella sua versione con alfabeto farfallino
 function butterlfyLetters(string){
 
     const stringA = string.replace('a', 'afa');
@@ -29,6 +52,16 @@ function butterlfyLetters(string){
     const newString = stringU
 
     return newString
-}
+};
+
+
+
+// funzione per recuperare le lettere comprese negli indici indcati (le lettere negli indici sono compresi nelle lettere)
+function lettersOfInfiniteString(string, indexStart, indexEnd){
+
+    const myString = string.substring(indexStart-1, indexEnd+1)
+
+    return myString
+};
 
 // ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ Mie Funzioni ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ \\
