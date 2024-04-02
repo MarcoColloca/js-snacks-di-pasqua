9d21f8e201197cd8fa6dfdc3e5a0e3100fb7a3c5
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-02 14:35:26 +0200

    Feat → added function to transform a string into a new string with butterlfy alphabet

--- js/app.js
+++ js/app.js
@@ -5,10 +5,18 @@ console.log('JS Test');
     Data una stringa, restituire la versione in alfabeto farfallino (le vocali devono essere trasformate in questo modo: a -> afa, e -> efe, i -> ifi, o -> ofo, u- > ufu)
 */
 
+const string = 'giallo'
+console.log(string)
+
+const stringA = string.replace('a', 'afa');
+const stringE = stringA.replace('e', 'efe');
+const stringI = stringE.replace('i', 'ifi');
+const stringO = stringI.replace('o', 'ofo');
+const stringU = stringO.replace('u', 'ufu');
 
 
 
-
+replaceLetters(string)
 
 
 
@@ -16,5 +24,17 @@ console.log('JS Test');
 
 // ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ Mie Funzioni ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ \\
 
+function replaceLetters(string){
+
+    const stringA = string.replace('a', 'afa');
+    const stringE = stringA.replace('e', 'efe');
+    const stringI = stringE.replace('i', 'ifi');
+    const stringO = stringI.replace('o', 'ofo');
+    const stringU = stringO.replace('u', 'ufu');
+
+    const newString = stringU
+
+    return console.log(newString)
+}
 
 // ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ Mie Funzioni ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ \\
